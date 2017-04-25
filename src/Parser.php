f0cf33df9bebc137d3953cbf9dc4a0da7654629f
Junior Grossi <juniorgro@gmail.com>
2017-04-25 14:26:02 -0300

    Initial commit

---
+++ src/Parser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Galahad\Bbcode;
+
+/**
+ * Class Parser
+ *
+ * @package Galahad\Bbcode
+ * @author Junior Grossi <juniorgro@gmail.com>
+ */
+class Parser
+{
+    public function __construct()
+    {
+        //
+    }
+
+    public function toHtml()
+    {
+        //
+    }
+
+    public function toBbcode()
+    {
+        //
+    }
+}
