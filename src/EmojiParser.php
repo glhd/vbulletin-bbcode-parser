c7c8c4d16a61508272d25537ee1d3475be8a6c91
Junior Grossi <juniorgro@gmail.com>
2017-05-31 15:40:13 -0300

    Parse emoticons using the Emojione library

--- src/EmojiParser.php
+++ src/EmojiParser.php
@@ -2,6 +2,8 @@
 
 namespace Galahad\Bbcode;
 
+use Emojione\Client;
+
 /**
  * Class EmojiParser
  *
@@ -10,6 +12,29 @@ namespace Galahad\Bbcode;
  */
 class EmojiParser
 {
+    const EMOJI_PATH = 'https://cdnjs.cloudflare.com/ajax/libs/emojione/2.2.7/assets';
+
+    /**
+     * @var Client
+     */
+    protected $emojione;
+
+    public function __construct()
+    {
+        $this->emojione = $this->createEmojione();
+    }
+
+    /**
+     * @param string $text
+     * @return string
+     */
+    public function parse($text)
+    {
+        $text = $this->defaultTranslations($text);
+
+        return $this->emojione->shortnameToImage($text);
+    }
+
     /**
      * @param string $content
      * @return string
@@ -28,5 +53,17 @@ class EmojiParser
         return str_replace(
             array_keys($translations), array_values($translations), $content
         );
+    }
+
+    /**
+     * @return Client
+     */
+    private function createEmojione()
+    {
+        $client =  new Client(new Ruleset());
+        $client->ascii = true;
+        $client->imagePathPNG = static::EMOJI_PATH;
+
+        return $client;
     }
 }
